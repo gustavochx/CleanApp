7d6c75f4bdf60f1b17850bcf6262bb0bb95102d6
gustavo.soares <gustavo.soares@cabal.com.br>
2021-02-23 22:20:00 -0300

    change HttpClient protocol to HttpPostClient for segregation interface inside tests

--- DataTests/RemoteAddAccountTests.swift
+++ DataTests/RemoteAddAccountTests.swift
@@ -10,9 +10,9 @@ import XCTest
 class RemoteAddAccount {
 
     private let url: URL
-    private let httpClient: HttpClient
+    private let httpClient: HttpPostClient
 
-    init(url: URL, httpClient: HttpClient) {
+    init(url: URL, httpClient: HttpPostClient) {
         self.url = url
         self.httpClient = httpClient
     }
@@ -22,8 +22,12 @@ class RemoteAddAccount {
     }
 }
 
-protocol HttpClient {
+protocol HttpPostClient {
     func post(url: URL)
+}
+
+protocol HttpClientGet {
+    func get(url: URL)
 }
 
 class RemoteAddAccountTests: XCTestCase {
@@ -35,12 +39,12 @@ class RemoteAddAccountTests: XCTestCase {
         XCTAssertEqual(httpClientSpy.url, url)
     }
 
-    class HttpClientSpy: HttpClient {
+    class HttpClientSpy: HttpPostClient {
+
         var url: URL?
 
         func post(url: URL) {
             self.url = url
         }
     }
-
 }
