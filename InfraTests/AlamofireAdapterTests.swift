3f96b52044187c0122ee7eebeaf8ee85a9f6fc16
gustavo.soares <gustavo.soares@cabal.com.br>
2021-10-24 17:17:33 -0300

    including Alamofire and creating infra framework no project with tests

---
+++ InfraTests/AlamofireAdapterTests.swift
@@ -0,0 +1,23 @@
+//
+//  InfraTests.swift
+//  InfraTests
+//
+//  Created by Gustavo Henrique Frota Soares on 24/10/21.
+//
+
+import XCTest
+import Alamofire
+
+
+class AlamofireAdapter {
+    
+}
+
+
+class AlamofireAdapterTests: XCTestCase {
+
+    func test_example() {
+        
+    }
+
+}
