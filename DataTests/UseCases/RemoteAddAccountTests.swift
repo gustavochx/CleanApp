e8a0a6f6feec4ff2b72144da1ed9d64e353a5367
gustavo.soares <gustavo.soares@cabal.com.br>
2021-10-24 15:56:25 -0300

    Creating test verifying if user changing view or just close app callback response not return values for deallocated instance

--- DataTests/UseCases/RemoteAddAccountTests.swift
+++ DataTests/UseCases/RemoteAddAccountTests.swift
@@ -54,7 +54,17 @@ class RemoteAddAccountTests: XCTestCase {
             httpClientSpy.completeWithData(makeInvalidData())
         }
     }
-
+    
+    func test_add_should_not_complete_if_sut_has_been_deallocated() {
+        let httpClientSpy = HttpClientSpy()
+        var sut: RemoteAddAccount? = RemoteAddAccount(url: makeUrl(), httpClient: httpClientSpy)
+        var result: Result<AccountModel, DomainError>?
+        
+        sut?.add(addAcountModel: makeAddAccountModel()) { result = $0 }
+        sut = nil        
+        httpClientSpy.completeWithError(.noConnectivity)
+        XCTAssertNil(result)
+    }
 }
 
 extension RemoteAddAccountTests {
