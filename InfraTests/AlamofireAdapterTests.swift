d5d33612b3361645f46ee03cc9f185d289d788b0
gustavo.soares <gustavo.soares@cabal.com.br>
2021-10-24 17:33:12 -0300

    Creating UrlProtocolStub for transform adapter methods be generic for some cases

--- InfraTests/AlamofireAdapterTests.swift
+++ InfraTests/AlamofireAdapterTests.swift
@@ -9,17 +9,48 @@ import XCTest
 import Alamofire
 
 class AlamofireAdapter {
+    
+    private let session: Session
+    
+    init(session: Session = .default) {
+        self.session = session
+    }
+    
     func post(to url: URL) {
-        // MARK: doing something later
-        
+        session.request(url).resume()
     }
 }
 
 class AlamofireAdapterTests: XCTestCase {
 
-    func test_example() {
-        let sut = AlamofireAdapter()
-        sut.post(to: URL())
-    }
+    func test_() {
 
+        let testingUrl = makeUrl()
+        
+        let sessionConfiguration = URLSessionConfiguration.default
+        sessionConfiguration.protocolClasses = [UrlProtocolStub.self]
+
+        let sut = AlamofireAdapter(session: Session(configuration: sessionConfiguration))
+        sut.post(to: testingUrl)
+        
+    }
+}
+
+class UrlProtocolStub: URLProtocol {
+    
+    override class func canInit(with request: URLRequest) -> Bool {
+        true
+    }
+    
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        request
+    }
+    
+    override func startLoading() {
+        
+    }
+    
+    override func stopLoading() {
+        
+    }
 }
