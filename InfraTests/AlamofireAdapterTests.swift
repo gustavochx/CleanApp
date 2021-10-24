295489e174b4f91f2f187d60c024c33c2b88fbce
gustavo.soares <gustavo.soares@cabal.com.br>
2021-10-24 17:18:44 -0300

    Creating post method inside AlamofireAdapter

--- InfraTests/AlamofireAdapterTests.swift
+++ InfraTests/AlamofireAdapterTests.swift
@@ -8,16 +8,18 @@
 import XCTest
 import Alamofire
 
-
 class AlamofireAdapter {
-    
+    func post(to url: URL) {
+        // MARK: doing something later
+        
+    }
 }
-
 
 class AlamofireAdapterTests: XCTestCase {
 
     func test_example() {
-        
+        let sut = AlamofireAdapter()
+        sut.post(to: URL())
     }
 
 }
